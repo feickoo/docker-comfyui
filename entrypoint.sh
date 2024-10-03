7b83f7ee0cef140993319dc51957f888c24280c8
feickoo <fei.ckoo@gmail.com>
2024-10-03 20:57:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ TEMP_DIR="/gitclone"
 
 # Check if /comfyui is a git repository
 if [ -d "${ROOT}/.git" ]; then
-  echo "checking updates for ComfyUI..."
+  echo "Checking updates for ComfyUI..."
   
   # Move into the /comfyui directory
   cd ${ROOT}
@@ -14,8 +14,8 @@ if [ -d "${ROOT}/.git" ]; then
   git fetch --tags
   
   # Get the latest local and remote tags
-  LOCAL_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
-  REMOTE_TAG=$(git describe --tags $(git ls-remote --tags origin | cut -d/ -f3 | tail -n1))
+  LOCAL_TAG=$(git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null || git rev-parse --short HEAD)
+  REMOTE_TAG=$(git ls-remote --tags origin | cut -d/ -f3 | tail -n1)
 
   echo "Current version (local): ${LOCAL_TAG}"
   echo "Latest version (remote): ${REMOTE_TAG}"
@@ -27,19 +27,45 @@ if [ -d "${ROOT}/.git" ]; then
     git pull --rebase
     echo "Updated to version ${REMOTE_TAG}"
   else
-    echo "Already up-to-date with the latest version."
+    echo "ComfyUI is up-to-date."
   fi
 else
   echo "Installing ComfyUI, cloning..."
   
   # Clone the repository into /comfyui and include the .git directory
-  git clone https://github.com/comfyanonymous/ComfyUI.git ${TEMP_DIR} --depth 1 --tags
+  git clone https://github.com/comfyanonymous/ComfyUI.git ${TEMP_DIR} --depth 1
   cd ${ROOT}
   
   # Move the files from the temporary directory to /comfyui
   cp -rf ${TEMP_DIR}/* ${ROOT}/
   cp -rf ${TEMP_DIR}/.[^.]* ${ROOT}/ # Also copy hidden files
   echo "Cloned and moved files to /comfyui successfully."
+fi
+
+# Check if ComfyUI-Manager exists and update or clone it
+if [ -d "${ROOT}/custom_nodes/ComfyUI-Manager/.git" ]; then
+   echo "Checking updates for ComfyUI-Manager..."
+
+   cd ${ROOT}/custom_nodes/ComfyUI-Manager
+   git fetch --tags
+   LOCAL_TAG=$(git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null || git rev-parse --short HEAD)
+   REMOTE_TAG=$(git ls-remote --tags origin | cut -d/ -f3 | tail -n1)
+   echo "Current ComfyUI-Manager version (local): ${LOCAL_TAG}"
+   echo "Latest ComfyUI-Manager version (remote): ${REMOTE_TAG}"   
+   
+   # If the versions don't match, pull the latest changes
+   if [ "${LOCAL_TAG}" != "${REMOTE_TAG}" ]; then
+     echo "New version of ComfyUI-Manager available, pulling the latest changes..."
+     git reset --hard origin/master
+     git pull --rebase
+     echo "Updated ComfyUI-Manager to version ${REMOTE_TAG}"
+   else
+     echo "ComfyUI-Manager is up-to-date."
+   fi
+else
+   echo "ComfyUI-Manager not found, installing..."
+   git clone https://github.com/ltdrdata/ComfyUI-Manager.git ${ROOT}/custom_nodes/ComfyUI-Manager --depth 1
+   echo "ComfyUI-Manager installed successfully."
 fi
 
 # Execute any passed command (like starting main.py)
