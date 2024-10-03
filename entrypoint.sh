e57e1f6d5c77108a5859985dae2b9b64dcf640ce
feickoo <fei.ckoo@gmail.com>
2024-10-03 18:12:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,3 @@
 #!/bin/bash
 
-set -Eeuo pipefail
-
-declare -A MOUNTS
-
-MOUNTS["${ROOT}"]="/root"
-
-for to_path in "${!MOUNTS[@]}"; do
-  set -Eeuo pipefail
-  from_path="${MOUNTS[${to_path}]}"
-  rm -rf "${to_path}"
-  if [ ! -f "$from_path" ]; then
-    mkdir -vp "$from_path"
-  fi
-  mkdir -vp "$(dirname "${to_path}")"
-  ln -sT "${from_path}" "${to_path}"
-  echo Mounted $(basename "${from_path}")
-done
-
-# Check if main.py exists
-if [ ! -f "main.py" ]; then
-  echo "main.py not found, cloning..."
   git clone https://github.com/comfyanonymous/ComfyUI.git
-else
-  echo "main.py found." 
-fi
-
-
-exec "$@"
