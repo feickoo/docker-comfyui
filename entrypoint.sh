d1567cf801e738982ed99439a81a7ce49205db7b
feickoo <fei.ckoo@gmail.com>
2024-10-03 18:10:21 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,12 +26,5 @@ else
   echo "main.py found." 
 fi
 
-# Check if ComfyUI-Manager exists
-if [ ! -d /custom_nodes/ComfyUI-Manager ]; then
-  echo "ComfyUI-Manager not found, cloning..."
-  git clone https://github.com/ltdrdata/ComfyUI-Manager.git ${ROOT}/custom_nodes/ComfyUI-Manager --depth 1
-else
-  echo "ComfyUI-Manager found." 
-fi
 
 exec "$@"
