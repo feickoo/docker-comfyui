8c067c2945da0e1048f3dc3ea43f038276335022
feickoo <fei.ckoo@gmail.com>
2024-10-03 14:51:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,22 @@
 
 set -Eeuo pipefail
 
+# Check if main.py exists
+if [ ! -f "main.py" ]; then
+  echo "main.py not found, cloning..."
+  git clone https://github.com/comfyanonymous/ComfyUI.git ${ROOT} --depth 1 
+else
+  echo "main.py found." 
+fi
+
+# Check if ComfyUI-Manager exists
+if [ ! -d /custom_nodes/ComfyUI-Manager ]; then
+  echo "main.py not found, cloning..."
+  git clone https://github.com/ltdrdata/ComfyUI-Manager.git ${ROOT}/custom_nodes/ComfyUI-Manager --depth 1
+else
+  echo "ComfyUI-Manager found." 
+fi
+
 declare -A MOUNTS
 
 MOUNTS["${ROOT}"]="/root"
