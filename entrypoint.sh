d50b61c7c3239b9834a51c3948182d70858caa39
feickoo <fei.ckoo@gmail.com>
2024-10-03 17:39:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,36 @@
 
 set -Eeuo pipefail
 
-  git clone https://github.com/comfyanonymous/ComfyUI.git /workspace --depth 1
+declare -A MOUNTS
 
-  # Move contents of /workspace to ${ROOT}
-  echo "Moving files from /workspace to ${ROOT}..."
-  mv /workspace/* ${ROOT}/
-  mv /workspace/.[^.]* ${ROOT}/ # Move hidden files (like .git)
+MOUNTS["${ROOT}"]="/root"
+
+for to_path in "${!MOUNTS[@]}"; do
+  set -Eeuo pipefail
+  from_path="${MOUNTS[${to_path}]}"
+  rm -rf "${to_path}"
+  if [ ! -f "$from_path" ]; then
+    mkdir -vp "$from_path"
+  fi
+  mkdir -vp "$(dirname "${to_path}")"
+  ln -sT "${from_path}" "${to_path}"
+  echo Mounted $(basename "${from_path}")
+done
+
+# Check if main.py exists
+if [ ! -f "main.py" ]; then
+  echo "main.py not found, cloning..."
+  git clone https://github.com/comfyanonymous/ComfyUI.git ${ROOT} --depth 1 
+else
+  echo "main.py found." 
+fi
+
+# Check if ComfyUI-Manager exists
+if [ ! -d /custom_nodes/ComfyUI-Manager ]; then
+  echo "ComfyUI-Manager not found, cloning..."
+  git clone https://github.com/ltdrdata/ComfyUI-Manager.git ${ROOT}/custom_nodes/ComfyUI-Manager --depth 1
+else
+  echo "ComfyUI-Manager found." 
+fi
 
 exec "$@"
