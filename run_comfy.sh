a1eba126636aaf1e59273bd522c1d2845ee2d9e6
feickoo <fei.ckoo@gmail.com>
2024-10-11 05:44:17 +0800

    Update run_comfy.sh

--- run_comfy.sh
+++ run_comfy.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-run_comfy() {
+# Check if ComfyUI is installed
+if [ -f "${COMFY}/main.py" ]; then
+  echo "ComfyUI is already installed."
+else
+  echo "ComfyUI not found, installing..."
+  git clone ${COMFY_GIT} ${TEMP_DIR} --depth 1 -q
+  cp -rf ${TEMP_DIR}/* ${COMFY}/
+  cp -rf ${TEMP_DIR}/.[^.]* ${COMFY}/
+  echo "ComfyUI installed successfully."
+fi
 
-    if [ -f "${COMFY}/main.py" ]; then
-      echo "ComfyUI is already installed."
-    else
-      echo "ComfyUI not found, installing..."
-      git clone ${COMFY_GIT} ${TEMP_DIR} --depth 1 -q
-      cp -rf ${TEMP_DIR}/* ${COMFY}/
-      cp -rf ${TEMP_DIR}/.[^.]* ${COMFY}/
-      echo "ComfyUI installed successfully."
-    fi
+# Check if ComfyUI-Manager is installed
+if [ -d "${COMFY}/custom_nodes/ComfyUI-Manager" ]; then
+  echo "ComfyUI-Manager is already installed."
+else
+  git clone ${C_M_GIT} ${COMFY}/custom_nodes/ComfyUI-Manager --depth 1 -q
+  echo "ComfyUI-Manager installed successfully."
+fi
 
-    if [ -d "${COMFY}/custom_nodes/ComfyUI-Manager" ]; then
-      echo "ComfyUI-Manager is already installed."
-    else
-      git clone ${C_M_GIT} ${COMFY}/custom_nodes/ComfyUI-Manager --depth 1 -q
-      echo "ComfyUI-Manager installed successfully."
-    fi
+# Set permissions for the /comfyui directory
+chown -R ${APPUSER}:${APPUSER} ${COMFY}
 
-    # Set permissions for the /comfyui directory
-    chown -R ${APPUSER}:${APPUSER} ${COMFY}
-
-    # Start the ComfyUI service as APPUSER
-    exec gosu ${APPUSER} python -u ${COMFY}/main.py --listen 0.0.0.0
-}
+# Start the ComfyUI service as APPUSER
+exec gosu ${APPUSER} python -u ${COMFY}/main.py --listen 0.0.0.0
